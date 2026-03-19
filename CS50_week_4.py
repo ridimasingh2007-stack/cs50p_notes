5a16e9e253a1f8a5eb73ab0ffabf4ff6f7b7e3e5
Ridima Singh <ridimasingh2007@gmail.com>
2026-03-19 11:35:22 +0530

    Update CS50_week_4.py

--- CS50_week_4.py
+++ CS50_week_4.py
@@ -71,6 +71,6 @@
 #if len(sys.argv) != 2:
 #     sys.exit()
 #response = #requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
-o = response.json()
-for result in o["results"]:
-    print(result["trackName"])
+#o = response.json()
+#for result in o["results"]:
+#    print(result["trackName"])
