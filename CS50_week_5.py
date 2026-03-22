2cfa4fbdedc8cae3d6c1d8ebcd952b611d7c8550
Ridima Singh <ridimasingh2007@gmail.com>
2026-03-22 17:19:56 +0530

    Add files via upload

---
+++ CS50_week_5.py
@@ -0,0 +1,26 @@
+# Unit tests:--- testing individual units of the code, those units are mainly fucntions
+
+# def main():
+#     x = int(input("What's x? "))
+#     print("x squared is", square(x))
+# def square(n):
+#     return n + n # creating bug intentionally 
+
+# if __name__ == "__main__":
+#     main()
+
+# assert:-- claim that it's true, if not there's some error shown ## used in test_CS50_week_5.py
+
+# AssertionError:- if the assert claim is wrong this shows up
+
+# pytest:-- it's a lib which automates the testing of the code that is written, it comes with builtin conventions.
+def main():
+    name = input("What's your name? ")
+    print(hello(name))
+def hello(to="world"):
+    return f"hello, {to}" # not using print cause it doesn't give any value
+
+if __name__ == "__main__":
+    main()
+
+
