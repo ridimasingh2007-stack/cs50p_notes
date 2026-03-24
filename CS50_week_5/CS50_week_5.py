52ffb8c98d93497ec9a5f665b0b7dd1689eff6ab
Ridima Singh <ridimasingh2007@gmail.com>
2026-03-24 20:39:45 +0530

    Refactor comments and improve code clarity

--- CS50_week_5/CS50_week_5.py
+++ CS50_week_5/CS50_week_5.py
@@ -1,4 +1,4 @@
-# Unit tests:--- testing individual units of the code, those units are mainly fucntions
+# Unit tests:--- testing individual units of the code, those units are mainly functions
 
 # def main():
 #     x = int(input("What's x? "))
@@ -11,9 +11,9 @@
 
 # assert:-- claim that it's true, if not there's some error shown ## used in test_CS50_week_5.py
 
-# AssertionError:- if the assert claim is wrong this shows up
+# AssertionError:- if the assert claim is wrong, this shows up
 
-# pytest:-- it's a lib which automates the testing of the code that is written, it comes with builtin conventions.
+# pytest:-- it's a library that automates the testing of the code that is written, it comes with built-in conventions.
 def main():
     name = input("What's your name? ")
     print(hello(name))
